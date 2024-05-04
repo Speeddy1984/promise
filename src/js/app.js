aece1a502908ced79f8ea9c1ca6a0e80661b3fbf
Speeddy1984 <speeddy1984@yandex.ru>
2024-05-04 11:35:32 +0800

    sync await hw

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,24 @@
 import json from "./parser";
 import read from "./reader";
 
-export default class GameSavingLoader {
-  static load() {
-    return read()
-      .then((data) => json(data))
-      .then((parsedData) => {
-        const parsedJSON = JSON.parse(parsedData);
-        return {
-          id: parsedJSON.id,
-          created: parsedJSON.created,
-          userInfo: {
-            id: parsedJSON.userInfo.id,
-            name: parsedJSON.userInfo.name,
-            level: parsedJSON.userInfo.level,
-            points: parsedJSON.userInfo.points,
-          },
-        };
-      });
+(async () => {
+  try {
+    const data = await read();
+    const parsedData = await json(data);
+    const parsedJSON = JSON.parse(parsedData);
+    
+    return {
+      id: parsedJSON.id,
+      created: parsedJSON.created,
+      userInfo: {
+        id: parsedJSON.userInfo.id,
+        name: parsedJSON.userInfo.name,
+        level: parsedJSON.userInfo.level,
+        points: parsedJSON.userInfo.points,
+      },
+    };
+
+  } catch (error) {
+    throw new Error (error.message);
   }
-}
+})();
